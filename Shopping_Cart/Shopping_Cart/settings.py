605a91b89a6e922d03f3daf914414c2782bfb866
Md Golam Mostofa <golammostofa10001@gmail.com>
2023-09-30 17:46:19 +0600

    railway deployment

--- Shopping_Cart/Shopping_Cart/settings.py
+++ Shopping_Cart/Shopping_Cart/settings.py
@@ -126,11 +126,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (user-uploaded files)
 MEDIA_URL = '/media/'
